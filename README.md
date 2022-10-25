1bccd437caff1ea6bd612b5f38f3773d5655172c
Alix Silvert <alix.silvert@protonmail.com>
2022-10-25 17:48:00 +0200

    got further in EDA

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This repository is an example of prediction based on a diabetes questionaire dat
 - The second one will not be bound by such limitations, we will in the end compare precisions in both cases.
 
 ## Description of data
+Raw data countains 520 rows and 17 different observations.
+
+Note that several rows are duplicate of each others. There is only 251 unique rows. Each row is duplicated at least once.
+Although it might be due to several patients having exactly the same profile, this seems unlikely
+
+No missing data in the dataset.
+
+Except for age, all data is binary.
--- scripts/dataExploration.py
+++ scripts/dataExploration.py
@@ -1,1 +1,54 @@
-#Todo
+#################################
+###Loading data and Librairies###
+#################################
+import pandas as pd
+import scipy.stats as ss
+
+
+diabetes_raw = pd.read_csv("data/diabetes_data.csv", sep=";")
+
+################################################
+###Dataset shape, type and summary statistics###
+################################################
+print("shame of dataset is {}".format(diabetes_raw.shape))
+print("-"*20)
+print("Columns automaticly detected type is")
+print(diabetes_raw.dtypes)
+print("-"*20)
+print("output of describe is")
+print(diabetes_raw.describe())
+print("-"*20)
+print("Number unique values")
+print(diabetes_raw.nunique())
+
+###############################
+###Checking for missing data###
+###############################
+def numberna_in_col(c : pd.Series) -> int:
+    return(c.isna().sum())
+
+number_na_per_column = diabetes_raw.apply(numberna_in_col)
+print("-"*20)
+print("Missing data")
+print(number_na_per_column)
+
+###########################################
+###Features variability and correlations###
+###########################################
+
+##Transforming binary features into boolean class (except gender for clarity)
+numerical_features = "age"
+categorical_features = "gender"
+features_to_change  = { k:"bool" for k in diabetes_raw.columns if not(k in (numerical_features + categorical_features))}
+diabetes_df = diabetes_raw.astype(features_to_change)
+categorical_features = [c for c in diabetes_raw.columns if not (c in numerical_features)]
+
+##Computing Cramer's V (based on chisquare) to our categorical variables
+
+    # """ calculate Cramers V statistic for categorial-categorial association.
+    #     uses correction from Bergsma and Wicher,
+    #     Journal of the Korean Statistical Society 42 (2013): 323-328
+    # """
+
+test_crosstab = pd.crosstab(diabetes_df["gender"], diabetes_df["obesity"])
+chi2 = ss.chi2_contingency(confusion_matrix)[0]
