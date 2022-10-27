1441dba45003f67b9f4c55b5ab7216c39aba84ee
Alix Silvert <alix.silvert@protonmail.com>
2022-10-27 16:39:27 +0200

    chi square analysis

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # diabetes_prediction
 
 ## Aims of the project
-This repository is an example of prediction based on a diabetes questionaire dataset available [here](https://www.kaggle.com/datasets/andrewmvd/early-diabetes-classification). It will include a description of the data, its characteristics as well as two types of modeling.
+This repository is an example of prediction based on a diabetes questionaire dataset originally published in Islam M.M.F., Ferdousi R., Rahman S., Bushra H.Y. (2020) Likelihood Prediction of Diabetes at Early Stage Using Data Mining Techniques. In: Gupta M., Konar D., Bhattacharyya S., Biswas S. (eds) Computer Vision and Machine Intelligence in Medical Image Analysis. Advances in Intelligent Systems and Computing, vol 992. Springer, Singapore. [https://doi.org/10.1007/978-981-13-8798-2_12](https://doi.org/10.1007/978-981-13-8798-2_12).
+It will include a description of the data, its characteristics as well as two types of modeling.
 - The first one will aim to be easilly implementable by a human in a clinical condition, i.e. we're trying to obtain an algorithm a human can easily follow and understand.
 - The second one will not be bound by such limitations, we will in the end compare precisions in both cases.
 
@@ -14,3 +15,12 @@ Although it might be due to several patients having exactly the same profile, th
 No missing data in the dataset.
 
 Except for age, all data is binary.
+
+
+### Dependencies within data
+
+#### Categorical variables
+Using Cramer's V (which can be summarised as a measure of chi square scaled from 0 to 1) we can see that most categorical variables bring information not carried by a single others, with the exception of class, polyuria and polydipsia.
+![Cramer's V scores](Images/cramersVrelationship.png)
+
+As class is our target feature, looking at those three specificly is of interest to our analysis.
--- scripts/dataExploration.py
+++ scripts/dataExploration.py
@@ -3,7 +3,11 @@
 #################################
 import pandas as pd
 import scipy.stats as ss
-
+import itertools
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.cluster import hierarchy
 
 diabetes_raw = pd.read_csv("data/diabetes_data.csv", sep=";")
 
@@ -45,10 +49,49 @@ categorical_features = [c for c in diabetes_raw.columns if not (c in numerical_f
 
 ##Computing Cramer's V (based on chisquare) to our categorical variables
 
-    # """ calculate Cramers V statistic for categorial-categorial association.
-    #     uses correction from Bergsma and Wicher,
-    #     Journal of the Korean Statistical Society 42 (2013): 323-328
-    # """
+def cramersV(x : pd.Series, y: pd.Series) -> float :
+    """ calculate Cramers V statistic for categorial-categorial association.
+        uses correction from Bergsma and Wicher,
+        Journal of the Korean Statistical Society 42 (2013): 323-328
+    """
+    contigency_table  = pd.crosstab(x, y)
+    #Those are the elements of classic Cramer's V
+    n = contigency_table.sum().sum()
+    chi2 = ss.chi2_contingency(contigency_table)[0]
+    phi2 = chi2/n
+    k, r = contigency_table.shape
 
-test_crosstab = pd.crosstab(diabetes_df["gender"], diabetes_df["obesity"])
-chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    #Computation of the unbiased versions
+    phi2 = max(0, phi2 - ((k-1)*(r-1)/(n-1)))
+    k = k - ((k-1)**2 /(n-1))
+    r = r - ((r-1)**2 /(n-1))
+    V = np.sqrt(phi2/min(k-1, r-1))
+    return(V)
+
+
+cramer_info = pd.DataFrame(itertools.product(categorical_features, categorical_features), columns = ["cat1", "cat2"])
+
+def applyCramerV(lineOfCramerInfo) -> float :
+    return(cramersV(  diabetes_df.loc[:,lineOfCramerInfo.cat1],  diabetes_df.loc[:,lineOfCramerInfo.cat2]))
+
+cramer_info["cramer"] = cramer_info.apply(applyCramerV,axis=1)
+cramer_info.cat1 = cramer_info.cat1.apply(lambda x : x.replace("_", " "))
+cramer_info.cat2 = cramer_info.cat2.apply(lambda x : x.replace("_", " "))
+cramer_info = cramer_info.pivot(index = "cat1", columns = "cat2", values = "cramer")
+
+## We use a hierarchical clustering to order nicely the features, but this clustering in itself does not carry much value
+Z = hierarchy.ward(cramer_info)
+leaf_order = hierarchy.leaves_list(hierarchy.optimal_leaf_ordering(Z, cramer_info)).tolist()
+leaf_order = [cramer_info.columns.tolist()[i] for i in leaf_order]
+
+cramer_info = cramer_info.reindex(leaf_order)
+cramer_info = cramer_info[leaf_order]
+
+
+plt.figure()
+ax = sns.heatmap(cramer_info, cmap="YlOrRd")
+plt.title("Cramer's V relationship")
+ax.set_ylabel('')
+ax.set_xlabel('')
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.savefig("Images/cramersVrelationship.png", bbox_inches = "tight")
